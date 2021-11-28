8a4a6ce702b8e73029761ece9b7d9b96dc5c7844
Neil Copland <neil.copland@gmail.com>
2021-11-28 12:31:05 +0000

    validate transaction and wallet create transaction

--- wallet/index.js
+++ wallet/index.js
@@ -1,6 +1,6 @@
 const {STARTING_BALANCE} = require('../config');
-const cryptoHash = require('../util/crypto-hash');
-const {ec} = require('../util');
+const {ec, cryptoHash} = require('../util');
+const Transaction = require('./transaction')
 
 class Wallet {
     constructor(){
@@ -13,6 +13,14 @@ class Wallet {
 
     sign(data) {
         return this.keyPair.sign(cryptoHash(data));
+    }
+
+    createTransaction({recipient, amount}) {
+        if(amount > this.balance) {
+            throw new Error("Amount exceeds balance");
+        }
+        return new Transaction({senderWallet: this, recipient, amount});
+
     }
 }
 
