96435d5f4d8d0e2378ec5cfac35a1e84bb1c69cd
Neil Copland <neil.copland@gmail.com>
2021-12-11 20:18:29 +0000

    add rewardTransaction to transaction.js

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -1,6 +1,7 @@
 const Wallet = require('.');
 const { verifySignature } = require('../util');
 const Transaction = require('./transaction');
+const {REWARD_INPUT, MINING_REWARD} = require('../config');
 
 describe('Transaction', () => {
     let transaction, senderWallet, recipient, amount;
@@ -163,5 +164,22 @@ describe('Transaction', () => {
         })
 
 
+    })
+
+    describe('rewardTransaction()', () => {
+        let rewardTransaction, minerWallet;
+
+        beforeEach(() => {
+            minerWallet = new Wallet();
+            rewardTransaction = Transaction.rewardTransaction({minerWallet});
+        });
+
+        it('creates a transaction with the reward input', () => {
+            expect(rewardTransaction.input).toEqual(REWARD_INPUT);
+        });
+
+        it('creates one transaction for the miner with the `MINING_REWARD`', () => {
+            expect(rewardTransaction.outputMap[minerWallet.publicKey]).toEqual(MINING_REWARD);
+        });
     })
 });
