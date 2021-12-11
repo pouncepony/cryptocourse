4d62f13ac2d91f1d040b352a03968e189981bacc
Neil Copland <neil.copland@gmail.com>
2021-12-11 20:30:29 +0000

    add clearBlockchainTransactions method to pool

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -2,6 +2,7 @@ const TransactionPool = require('./transaction-pool');
 const Transaction = require('./transaction');
 const Wallet = require('./index');
 const { validTransaction } = require('./transaction');
+const Blockchain = require('../blockchain')
 
 describe("TransactionPool", () => {
     let transactionPool, transaction, senderWallet;
@@ -74,4 +75,36 @@ describe("TransactionPool", () => {
 
     });
     
+    describe('clear()', ()=> {
+        it('clears the transactions', ()=>{
+            transactionPool.clear();
+
+            expect(transactionPool.transactionMap).toEqual({});
+        })
+    })
+
+    describe('clearBlockchainTransactions()', () => {
+        it('clears the pool of any existing blockchain transactions', () => {
+            const blockchain = new Blockchain();
+            const expectedTransactionMap = {};
+
+            for(let i=0; i<6;i++) {
+                const transaction = new Wallet().createTransaction({
+                    recipient: 'foo', amount: 20
+                });
+
+                transactionPool.setTransaction(transaction);
+
+                if(i%2===0) {
+                    blockchain.addBlock({ data: [transaction]})
+                } else {
+                    expectedTransactionMap[transaction.id] = transaction;
+                }
+            }
+
+            transactionPool.clearBlockchainTransactions({chain: blockchain.chain});
+
+            expect(transactionPool.transactionMap).toEqual(expectedTransactionMap);
+        })
+    })
 })
