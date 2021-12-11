38bf78a8e1f31640d2e0a1bac2195c4785c9a33f
Neil Copland <neil.copland@gmail.com>
2021-12-11 20:43:46 +0000

    implement transaction-miner

--- app/transaction-miner.js
+++ app/transaction-miner.js
@@ -1,3 +1,5 @@
+const Transaction = require('../wallet/transaction');
+
 class TransactionMiner {
     constructor({blockchain, transactionPool, wallet, pubsub}) {
         this.blockchain = blockchain;
@@ -5,16 +7,18 @@ class TransactionMiner {
         this.wallet = wallet;
         this.pubsub = pubsub;
     }
-    mineTransaction() {
-        //get the transaction pool's valid transactions
+    mineTransactions() {
+        const validTransactions = this.transactionPool.validTransactions();
         
-        //generate the miner's reward
+        validTransactions.push(
+            Transaction.rewardTransaction({minerWallet: this.wallet})
+        );
 
-        // adda block consisting of these transacitons to the blockchain
+        this.blockchain.addBlock({data: validTransactions});
 
-        //broadcast the updated blockchain
+        this.pubsub.broadcastChain();
 
-        //clear the pool
+        this.transactionPool.clear();
     }
 }
 
