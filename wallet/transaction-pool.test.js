2e5b3d1089c113bc45d05586b0849e962c62ad7b
Neil Copland <neil.copland@gmail.com>
2021-12-11 10:00:16 +0000

    transaction pool check existing and get endpoint

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -3,15 +3,16 @@ const Transaction = require('./transaction');
 const Wallet = require('./index');
 
 describe("TransactionPool", () => {
-    let transactionPool, transaction;
+    let transactionPool, transaction, senderWallet;
 
     beforeEach(()=>{
         transactionPool = new TransactionPool();
+        senderWallet = new Wallet();
         transaction = new Transaction({
-            senderWallet: new Wallet(),
+            senderWallet,
             recipient: 'fake-recipient',
             amount: 50
-        })
+        });
     });
 
     describe('setTransaction()', ()=>{
@@ -23,6 +24,15 @@ describe("TransactionPool", () => {
         });
     });
 
+    describe('existingTransaction()', ()=> {
+        it('returns an existing transaction given an input address', () => {
+            transactionPool.setTransaction(transaction);
+
+            expect(
+                transactionPool.existingTransaction({inputAddress: senderWallet.publicKey})
+            ).toBe(transaction);
+        })
+    })
 
     
 })
