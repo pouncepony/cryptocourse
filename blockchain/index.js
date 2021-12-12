4c154be34af586b62e7b8aab2a925b30e3316309
Neil Copland <neil.copland@gmail.com>
2021-12-12 08:47:26 +0000

    clear peer transaction pools

--- blockchain/index.js
+++ blockchain/index.js
@@ -15,7 +15,7 @@ class Blockchain {
         this.chain.push(newBlock)
     }
 
-    replaceChain(chain){
+    replaceChain(chain, onSuccess){
         if (chain.length <= this.chain.length) {
             console.error('The incoming chain must be longer');
             return;
@@ -25,7 +25,7 @@ class Blockchain {
             console.error('The incoming train must be valid');
             return;
         }
-
+        if(onSuccess) onSuccess();
         console.log('replacing chain with', chain);
         this.chain = chain;
     }
