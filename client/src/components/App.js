413c69447bf153363ecde095fa0b2e52110c7068
Neil Copland <neil.copland@gmail.com>
2021-12-22 19:45:48 +0000

    add transaction components and routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Blocks from './Blocks';
+import {Link} from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 class App extends Component {
@@ -19,15 +19,17 @@ class App extends Component {
             <div className="App">
                 <img className='logo' src={logo}></img>
                 <br/>
-                <div>Welcome to the blockchain!</div>
+                <div>
+                    Welcome to the blockchain!
+                </div>
                 <br/>
+                <div><Link to='/blocks'>Blocks</Link></div>
+                <br />
                 <div className='WalletInfo'>
                     <div>Address: {address}</div>
                     <div>Balance: {balance}</div>
                 </div>
-    
                 <br />
-                <Blocks/>
             </div>
         );
     }
